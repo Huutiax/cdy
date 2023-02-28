70874594622cfdc6343ccb394d67f9e88996d563
Fshenme <Huutiaxx@gmail.com>
2023-02-28 18:35:16 +0800

    study

---
+++ vue3-ts-cms(1-4)/src/views/login/cpns/login-account.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="login-account">
+    <el-form label-width="60px" :rules="rules" :model="account" ref="formRef">
+      <el-form-item label="账号" prop="name">
+        <el-input v-model="account.name" />
+      </el-form-item>
+      <el-form-item label="密码" prop="password">
+        <el-input v-model="account.password" />
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, ref } from 'vue'
+import { ElForm } from 'element-plus'
+
+import { rules } from '../config/account-config'
+
+export default defineComponent({
+  setup() {
+    const account = reactive({
+      name: '',
+      password: ''
+    })
+
+    const formRef = ref<InstanceType<typeof ElForm>>()
+
+    const loginAction = () => {
+      //value为绑定的组件对象 validate为验证，验证后有回调，回调里返回一个布尔类型
+      formRef.value?.validate((valid) => {
+        if (valid) {
+          console.log('真正执行登录逻辑')
+        }
+      })
+    }
+
+    return {
+      account,
+      rules,
+      loginAction,
+      formRef
+    }
+  }
+})
+</script>
+
+<style scoped></style>
---
+++ vue3-ts-cms11/src/store/types.ts
@@ -0,0 +1,16 @@
+import { ILoginState } from './login/types'
+import { ISystemState } from './main/system/types'
+
+export interface IRootState {
+  name: string
+  age: number
+  entireDepartment: any[]
+  entireRole: any[]
+}
+
+export interface IRootWithModule {
+  login: ILoginState
+  system: ISystemState
+}
+
+export type IStoreType = IRootState & IRootWithModule
